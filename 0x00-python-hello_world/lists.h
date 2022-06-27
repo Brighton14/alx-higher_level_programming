41403b171bb66f13595822953aeabd36d12a9fe6
Brighton <ekuayovwegreat@gmail.com>
2022-06-27 23:25:04 +0000

    Hello World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,21 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
-<<<<<<< HEAD:0x00-python-hello_world/lists.h
-=======
+
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- * Description: singly linked node structure
+ *
+ * Description: singly linked list node structure
+ * 
  */
->>>>>>> d22c9e4a3122e3d33e706ab7a152c46ce212a127:0x00-python-hello_world/list.h
-
-#include <stdlib.h>
-
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
